0029add7634d2795361bd173d7ae84c1160d2b9e
jk <jaswanth424@gmail.com>
2024-04-06 15:05:44 -0500

    Insert records and Retrieve records.

--- src/main/java/com/jk/jpa/oneToMany/controller/OrderController.java
+++ src/main/java/com/jk/jpa/oneToMany/controller/OrderController.java
@@ -1,14 +1,18 @@
 package com.jk.jpa.oneToMany.controller;
 
 
+import com.jk.jpa.oneToMany.dto.OrderResponse;
 import com.jk.jpa.oneToMany.model.Customer;
 import com.jk.jpa.oneToMany.repository.CustomerRepository;
 import com.jk.jpa.oneToMany.repository.ProductRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -23,4 +27,10 @@ public class OrderController {
     public Customer placeOrder(@RequestBody Customer customer){
         return customerRepository.save(customer);
     }
+
+    @GetMapping("/getOrder")
+    public List<OrderResponse> getJoinInformation(){
+        return customerRepository.getJoinInformation();
+    }
+
 }
--- src/main/java/com/jk/jpa/oneToMany/repository/CustomerRepository.java
+++ src/main/java/com/jk/jpa/oneToMany/repository/CustomerRepository.java
@@ -1,7 +1,15 @@
 package com.jk.jpa.oneToMany.repository;
 
+import com.jk.jpa.oneToMany.dto.OrderResponse;
 import com.jk.jpa.oneToMany.model.Customer;
+import org.aspectj.weaver.ast.Literal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    @Query("SELECT new com.jk.jpa.oneToMany.dto.OrderResponse (c.name, p.name) from Customer c JOIN c.products p")
+    public List<OrderResponse> getJoinInformation();
 }
